8225091646e659a0d5d39060677263eef1c9f5b0
toctoce <toctoce@naver.com>
2026-01-04 22:46:19 +0900

    fix(event): 호출할 때 할인 금액을 계산하도록 변경

--- src/main/java/christmas/vo/event/Event.java
+++ src/main/java/christmas/vo/event/Event.java
@@ -3,56 +3,52 @@ package christmas.vo.event;
 import christmas.vo.date.MyDate;
 import christmas.vo.order.Menu;
 import christmas.vo.order.Order;
-import java.util.List;
 
 /*
     items : [dDayDiscount, weekdayDiscount, weekendDiscount, specialDiscount, gift]
  */
-public record Event(List<Integer> items) {
+public record Event(Order order, MyDate date) {
 
-    public static Event from(Order order, MyDate date) {
-
-        if (order.getOriginalPrice() < 10000) {
-            return new Event(List.of(0, 0, 0, 0, 0));
-        }
-        int dDayDiscount = calculateDDayDiscount(date);
-        int weekdayDiscount = calculateWeekdayDiscount(order, date);
-        int weekendDiscount = calculateWeekendDiscount(order, date);
-        int specialDiscount = calculateSpecialDiscount(date);
-        int gift = calculateGift(order);
-
-        return new Event(List.of(dDayDiscount, weekdayDiscount, weekendDiscount, specialDiscount, gift));
+    private boolean isLowerThan10000() {
+        return order.getOriginalPrice() < 10000;
     }
 
-    private static int calculateWeekdayDiscount(Order order, MyDate date) {
-        if (!date.isWeekday()) {
+    public int getWeekdayDiscount() {
+        if (isLowerThan10000() || !date.isWeekday()) {
             return 0;
         }
         return order.getCategoryCount("디저트") * 2023;
     }
 
-    private static int calculateWeekendDiscount(Order order, MyDate date) {
-        if (!date.isWeekend()) {
+    public int getWeekendDiscount() {
+        if (isLowerThan10000() || !date.isWeekend()) {
             return 0;
         }
         return order.getCategoryCount("메인") * 2023;
     }
 
-    private static int calculateSpecialDiscount(MyDate date) {
-        if (!date.isSpecialDiscount()) {
+    public int getSpecialDiscount() {
+        if (isLowerThan10000() || !date.isSpecialDiscount()) {
             return 0;
         }
         return 1000;
     }
 
-    private static int calculateDDayDiscount(MyDate date) {
+    public int getDDayDiscount() {
+        if (isLowerThan10000() || !date.isDDayDiscount()) {
+            return 0;
+        }
         return 900 + date.date().getDayOfMonth() * 100;
     }
 
-    private static int calculateGift(Order order) {
-        if (order.getOriginalPrice() >= 120_000) {
-            return Menu.from("샴페인").getPrice();
+    public int getGift() {
+        if (isLowerThan10000() || order.getOriginalPrice() < 120_000) {
+            return 0;
         }
-        return 0;
+        return Menu.from("샴페인").getPrice();
+    }
+
+    public int getTotalDiscount() {
+        return getDDayDiscount() + getWeekdayDiscount() + getWeekendDiscount() + getSpecialDiscount() + getGift();
     }
 }
---
+++ src/test/java/christmas/vo/event/EventTest.java
@@ -0,0 +1,41 @@
+package christmas.vo.event;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.vo.date.MyDate;
+import christmas.vo.order.Order;
+import org.junit.jupiter.api.Test;
+
+class EventTest {
+
+    @Test
+    void 할인_가격_계산() {
+        Order order = Order.from("양송이수프-1,타파스-2,바비큐립-3,크리스마스파스타-2,초코케이크-3,제로콜라-1");
+        Event event7 = new Event(order, MyDate.from("7"));
+        Event event8 = new Event(order, MyDate.from("8"));
+        Event event25 = new Event(order, MyDate.from("25"));
+        Event event26 = new Event(order, MyDate.from("26"));
+
+        Order cheapOrder = Order.from("양송이수프-1,바비큐립-1");
+        Event cheapEvent = new Event(cheapOrder, MyDate.from("7"));
+
+        assertThat(event7.getDDayDiscount()).isEqualTo(1600);
+        assertThat(event8.getDDayDiscount()).isEqualTo(1700);
+        assertThat(event25.getDDayDiscount()).isEqualTo(3400);
+        assertThat(event26.getDDayDiscount()).isEqualTo(0);
+
+        assertThat(event7.getWeekdayDiscount()).isEqualTo(2023 * 3);
+        assertThat(event8.getWeekdayDiscount()).isEqualTo(0);
+
+        assertThat(event7.getWeekendDiscount()).isEqualTo(0);
+        assertThat(event8.getWeekendDiscount()).isEqualTo(2023 * 5);
+
+        assertThat(event25.getSpecialDiscount()).isEqualTo(1000);
+        assertThat(event26.getSpecialDiscount()).isEqualTo(0);
+
+        assertThat(event7.getGift()).isEqualTo(25000);
+        assertThat(cheapEvent.getGift()).isEqualTo(0);
+
+        assertThat(event7.getTotalDiscount()).isEqualTo(1600 + 2023 * 3 + 0 + 0 + 25000);
+    }
+}
