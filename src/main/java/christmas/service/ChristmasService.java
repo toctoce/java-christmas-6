9ae192069a952a664467cf7b0d7a8289da02a761
toctoce <toctoce@naver.com>
2026-01-05 00:01:20 +0900

    feat(dto): 출력용 dto 구현

--- src/main/java/christmas/service/ChristmasService.java
+++ src/main/java/christmas/service/ChristmasService.java
@@ -2,17 +2,13 @@ package christmas.service;
 
 import christmas.dto.ResultDto;
 import christmas.vo.date.MyDate;
+import christmas.vo.event.Event;
 import christmas.vo.order.Order;
 
 public class ChristmasService {
 
-    public ResultDto createSchedule(MyDate firstMyDate) {
-//        Schedule schedule = Schedule.from(firstDate);
-//        return ScheduleDto.from(schedule);
-        return null;
-    }
-
     public ResultDto createEvent(MyDate date, Order order) {
-        return null;
+        Event event = new Event(order, date);
+        return ResultDto.from(event);
     }
 }
